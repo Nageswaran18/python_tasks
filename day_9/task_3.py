4026f40ee819be6ed24c61ab6a4990942e507be4
Nageswaran18 <nageswarann18@gmail.com>
2023-09-03 21:35:33 -0700

    commited

--- day_9/task_3.py
+++ day_9/task_3.py
@@ -5,10 +5,11 @@ def read_and_write(source_file, target_file, source_encoding, target_encoding):
     with open(target_file, "w", encoding=target_encoding) as target:
         target.write(content)
 
+
 def main():
-    source_file = "C:\\Naga\\day_9\\en.txt"  # Modify this to the correct path if needed
-    target_file = "es_translated_to_en.txt"  # Provide just the filename
-    source_encoding = "iso-8859-1"  # Use the correct encoding for your file
+    source_file = "C:\\Naga\\day_9\\en.txt"  
+    target_file = "es_translated_to_en.txt"  
+    source_encoding = "iso-8859-1" 
     target_encoding = "utf-16"
     
     read_and_write(source_file, target_file, source_encoding, target_encoding)
